d015ee53f2ac52d972d85e7049bf8f1eaf438126
shizoleg <oleg.shiz@bk.ru>
2022-11-28 01:14:06 +0300

    update and animations

--- js/script.js
+++ js/script.js
@@ -4,3 +4,58 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 });
+
+// let buy = document.querySelector(".buy-button");
+document.querySelector(".buy-button").onclick = function() {
+
+    Swal.fire({
+        title: 'Для приобретения курса обращайтесь по нашим контактам:' +
+            '<br>WhatsApp: +89109122539' +
+            '<br>Facebook: facebook.com' +
+            '<br>Telegram: +89109122539',
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        }
+    })
+
+}
+
+document.querySelector(".whatsapp-button").onclick = function () {
+
+    Swal.fire({
+        title: 'WhatsApp: +89109122539',
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        }
+    })
+}
+document.querySelector(".facebook-button").onclick = function () {
+
+    Swal.fire({
+        title: 'Facebook: facebook.com',
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        }
+    })
+}
+document.querySelector(".telegram-button").onclick = function () {
+
+    Swal.fire({
+        title: 'Telegram: +89109122539',
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        }
+    })
+}
