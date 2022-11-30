a4833f5cc6b59bf712528013739de41c5b325d6e
shizoleg <oleg.shiz@bk.ru>
2022-11-30 15:36:40 +0300

    add mobile menu

--- js/script.js
+++ js/script.js
@@ -3,10 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
     let instances = M.Carousel.init(elems, {
 
     });
-});
 
-// let buy = document.querySelector(".buy-button");
-document.querySelector(".buy-button").onclick = function() {
+let button1 = document.getElementById("btn1");
+let button2 = document.getElementById("btn2");
+let button3 = document.getElementById("btn3");
+let button4 = document.getElementById("btn4");
+let button5 = document.getElementById("btn5");
+let button6 = document.getElementById("btn6");
+let button_whatsapp = document.getElementById("whatsapp-button");
+let button_telegram = document.getElementById("telegram-button");
+let button_facebook = document.getElementById("facebook-button");
+let buy_button = document.querySelector(".buy-button");
+// let bodyclk = document.querySelector("body");
+
+
+function buy(){
 
     Swal.fire({
         title: 'Для приобретения курса обращайтесь по нашим контактам:' +
@@ -23,8 +34,7 @@ document.querySelector(".buy-button").onclick = function() {
 
 }
 
-document.querySelector(".whatsapp-button").onclick = function () {
-
+function whatsapp(){
     Swal.fire({
         title: 'WhatsApp: +89109122539',
         showClass: {
@@ -35,7 +45,7 @@ document.querySelector(".whatsapp-button").onclick = function () {
         }
     })
 }
-document.querySelector(".facebook-button").onclick = function () {
+function facebook(){
 
     Swal.fire({
         title: 'Facebook: facebook.com',
@@ -47,7 +57,7 @@ document.querySelector(".facebook-button").onclick = function () {
         }
     })
 }
-document.querySelector(".telegram-button").onclick = function () {
+function telegram(){
 
     Swal.fire({
         title: 'Telegram: +89109122539',
@@ -59,3 +69,24 @@ document.querySelector(".telegram-button").onclick = function () {
         }
     })
 }
+
+function menuclk() {
+    document.getElementById('active').checked = false;
+
+}
+
+
+button1.addEventListener("click", menuclk);
+button2.addEventListener("click", menuclk);
+button3.addEventListener("click", menuclk);
+button4.addEventListener("click", menuclk);
+button5.addEventListener("click", menuclk);
+button6.addEventListener("click", menuclk);
+button_whatsapp.addEventListener("click", whatsapp);
+button_facebook.addEventListener("click", facebook);
+button_telegram.addEventListener("click", telegram);
+buy_button.addEventListener("click", bye);
+// bodyclk.addEventListener("click", menuclk);
+
+
+});
